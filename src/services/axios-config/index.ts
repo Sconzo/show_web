c848d05f9682e836fd087ace264b3d81a11116fd
Rodrigo Polydoro <rspolydoro@gmail.com>
2023-03-22 14:20:20 -0300

    Test router

--- src/services/axios-config/index.ts
+++ src/services/axios-config/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { errorInterceptor, responseInterceptor } from './interceptors';
 
 const Api = axios.create({
-    baseURL:"http://localhost:8080/",
+    baseURL:"https://young-thor-beige-morocco.bohr.io/api/",
     responseType:"json",
     headers:{
         "Content-Type": "application/json",
