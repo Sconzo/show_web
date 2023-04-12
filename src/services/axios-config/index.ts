dd13603682f25ce4eccc03ef0ea678a17625b782
Rodrigo Polydoro <81029795+Sconzo@users.noreply.github.com>
2023-04-11 21:21:24 -0300

    Revert "CORS"

--- src/services/axios-config/index.ts
+++ src/services/axios-config/index.ts
@@ -2,12 +2,15 @@ import axios from 'axios'
 import { errorInterceptor, responseInterceptor } from './interceptors';
 
 const Api = axios.create({
-  baseURL:"https://young-thor-beige-morocco.bohr.io/api/",
   responseType:"json",
   headers:{
     "Content-Type": "application/json",
   }
 });
 
+Api.interceptors.response.use(
+    (response) => responseInterceptor(response),
+    (error) => errorInterceptor(error),
+)
 
 export { Api }
