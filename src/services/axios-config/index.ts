cd6bf7c75fc2f17ba413d4bbbb7e0b3699e1dbe8
Rodrigo Polydoro <rspolydoro@gmail.com>
2023-04-11 21:28:51 -0300

    CORS

--- src/services/axios-config/index.ts
+++ src/services/axios-config/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { errorInterceptor, responseInterceptor } from './interceptors';
 
 const Api = axios.create({
+  baseURL:"https://great-volleyball-gold-ireland.bohr.io",
   responseType:"json",
   headers:{
     "Content-Type": "application/json",
